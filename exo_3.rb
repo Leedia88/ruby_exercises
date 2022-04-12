67da6beaff7bf61294ed2757353b7c7fee43a5bf
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-04-12 13:19:55 +0200

    nouvelles modif

--- exo_3.rb
+++ exo_3.rb
@@ -1,11 +1,12 @@
 puts "Exercice 3"
 print "Quel est ton année de naissance? >"
 annee = gets.chomp.to_i
-if (2017 - annee) < 20
+age = 2017 - annee
+if (age) < 20
   puts "Wow, tu es super jeune!"
-elsif (2017 - annee) < 30
+elsif (age) < 30
   puts "Le bel âge!"
-elsif (2017 - annee) < 30
+elsif (age) < 30
   puts "La force de l'âge ET la sagesse, au top!"
 end
-puts "Au fait, tu avais #{2017 - annee} ans en 2017 :)"
+puts "Au fait, tu avais #{age} ans en 2017 :)"
--- exo_9.rb
+++ exo_9.rb
@@ -1,6 +1,7 @@
 puts "Exercice 9"
 print "Désolée, j'ai encore oublié, quelle est ton année de naissance déjà? >"
 nombre = gets.chomp.to_i
-(Time.new.year - nombre +1 ).times do |i|
+annee = Time.new.year
+(annee - nombre +1 ).times do |i|
   print "#{nombre + i}; "
 end
