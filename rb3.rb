4d02caf0bc9e181a5e6ceea37367df93e7697085
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-04-12 15:16:57 +0200

    ajout fichier rb3

---
+++ rb3.rb
@@ -0,0 +1,38 @@
+# print "io"
+# puts
+# print "ki"
+# puts "\n"
+
+
+###################### Boucle for
+
+
+# liste = [1,2,3,4,5]
+# # liste_2 = []
+# # for item in liste
+# #     puts item
+# # end
+
+# for index in 0..4
+#     puts liste[index]
+# end
+
+# liste.each do |item|
+#     puts item
+# end
+
+
+
+#######################  HASH // Dictionnaries
+
+options = {:font_size => 15, :font_family => "Arial"}
+puts options[:font_family]
+
+options[:background] = "red"
+puts options
+
+
+#######################  
+
+
+
