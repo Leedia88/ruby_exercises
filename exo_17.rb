b396247e4d659315418b22db2699f57765ead5bf
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-04-12 11:52:49 +0200

    ajout corrections suite revue groupe

--- exo_17.rb
+++ exo_17.rb
@@ -1,16 +1,22 @@
 puts "~Exercice 17"
 print "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ? (entre 1 et 25 stp!) >"
 nombre = gets.chomp.to_i
-puts "Voici la pyramide :"
-nombre.times do |i|
-  (nombre - i + 1).times do
-    print " "
+if nombre < 26
+
+  puts "Voici la pyramide :"
+  nombre.times do |i|
+    (nombre - i + 1).times do
+      print " "
+    end
+    (i + 1).times do
+      print "#"
+    end
+    (i).times do
+      print "#"
+    end
+    puts "\n"
   end
-  (i + 1).times do
-    print "#"
-  end
-  (i).times do
-    print "#"
-  end
-  puts "\n"
+
+else
+  puts "Ce n'est pas le nombre demandé! Recommence stp"
 end
