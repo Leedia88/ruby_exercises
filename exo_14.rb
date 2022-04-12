841b5c1e213351abac2946a8bc962783c0df54ef
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-04-12 15:55:44 +0200

    modif exo 14

--- exo_14.rb
+++ exo_14.rb
@@ -1,16 +1,13 @@
 puts "Exercice 14"
 
-# liste_pairs=[]
-# nombre = 50
-# (nombre/2).times do |i|
-#   puts "jean.dupont." + (2*(i + 1)/10%10).to_s +  (2*(i+1)%10).to_s + "@email.fr"
-# end
-
-liste_emails = Array.new
+liste_emails = []
 50.times do |i|
-  adresse = "jean.dupont." + (((i+1)/10)%10).to_s + ((i+1)%10).to_s + "@email.fr"
+  unite = ((i+1)%10)
+  dizaine = (((i+1)/10)%10)
+  adresse = "jean.dupont." + dizaine.to_s + unite.to_s + "@email.fr"
   liste_emails.push(adresse)
-  if ((i+1)%10)%2 == 0
+
+  if unite % 2 == 0
     puts adresse
   end
 end
